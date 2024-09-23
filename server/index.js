83d8698f19569645eeb14314d06f48557ea6c7b0
tkiet002 <tuankietgam2505@gmail.com>
2024-09-23 12:07:57 +0700

    fixed vercel.json and add require mysql2

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { Sequelize } = require("sequelize");
+const { mysql2 } = require("mysql2");
 const app = express();
 const port = 3001;
 
